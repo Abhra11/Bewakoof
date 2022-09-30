6fb38864c0d98dbaf7c7bd7dcb2f83c66186cfa5
Abhra11 <abhra001597@gmail.com>
2022-09-30 23:29:28 +0530

    day 4

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import Header from '../Components/Header'
+import "../styles/Product.css"
 
 function Product(){
     return <>
-        <h1>hi</h1>
+    <Header/>
+
+    <div className="parent">
+        <p style={{'fontSize':'12px', 'marginTop':'10px'}}>Home  /  Men Clothing</p>
+        <p style={{'fontSize':'24px', 'marginTop':'24px', fontWeight:'bold', borderBottom:'1px solid', width:'20%'}}>Men Clothing (15)</p>
+        <div>
+            <div>
+
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
+    </div>
+
+        
     </>
 }
 
